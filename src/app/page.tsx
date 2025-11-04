63635e5cb0c9cc9eaccf6c6a84e31dc269a00fe7
muhammadnisarbsse7 <muhammadnisarbsse7@gmail.com>
2025-11-04 11:13:37 +0500

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 // import Image from "next/image";
 
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { Progress } from '@radix-ui/react-progress';
 
 export default function Home() {
   return (
     <>
-      <Button>Hello</Button>
+      <Button variant="elevated">Hello</Button>
+      <Input value={'Enter text here'}></Input>
+      <Progress value={50} />
+      <Textarea placeholder="fff" />
     </>
   );
 }
